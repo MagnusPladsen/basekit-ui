b733bf2b77c313b787fb7e0d9304ff02665bda9f
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2024-03-29 12:23:51 +0100

    Delete dist directory

--- dist/esm/types/components/Button/Button.types.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface BKButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-    variant?: BKButtonVariant;
-    href?: string;
-}
-type BKButtonVariant = "contained" | "outlined" | "text";
-export type { BKButtonProps, BKButtonVariant };
